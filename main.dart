e1cae5a88ea41ffd00ee93253c29c3a09ef844d8
smm <73750388+hicte@users.noreply.github.com>
2021-03-04 04:28:35 +0330

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi' as ffi;
+
+typedef SystemC = ffi.Int32 Function(ffi.Int32, ffi.Int32);
+typedef SystemDart = int Function(int, int);
+void main() {
+  // change the path for other platforms (Mac, Windows)
+  const sharedLibPath = './shared_lib.so';
+  const addFuncName = 'add';
+  const powerFuncName = 'power';
+
+  final sharedLib = ffi.DynamicLibrary.open(sharedLibPath);
+
+  final add = sharedLib.lookupFunction<SystemC, SystemDart>(addFuncName);
+  final power = sharedLib.lookupFunction<SystemC, SystemDart>(powerFuncName);
+
+  int onePlusTow = add(1, 2);
+  int towPowerThree = power(2, 3);
+
+  print(onePlusTow);
+  print(towPowerThree);
+}
---
+++ my_lib.c
@@ -0,0 +1,4 @@
+#include <math.h>
+
+int add(int a, int b) { return a + b; }
+int power(int x, int y) { return (int)pow(x, y); }
